cfced72e9564c9e663a93d41a91ce5dde33a18d1
camel <157693710+coco-camel@users.noreply.github.com>
2024-02-14 16:33:33 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 4.29 ms, Memory: 36 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,16 @@
+function solution(left, right) {
+    var answer = 0;
+    for (left; left <= right; left++) {
+        console.log(solution1(left));
+        answer += solution1(left);
+    }
+    return answer;
+}
+
+function solution1(num) {
+    let result = [];
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) result.push(i);
+    }
+    return result.length % 2 === 0 ? num : -num;
+}
