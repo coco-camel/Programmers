63ae449f0ed8cac2d84f82faca9dde4769b698f1
camel <157693710+coco-camel@users.noreply.github.com>
2024-02-14 19:30:25 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.47 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,10 @@
 function solution(arr1, arr2) {
-    let answer = [];
+    let answer = [[]];
     for (let i = 0; i < arr1.length; i++) {
-        let newArr1 = [];
+        answer[i] = [];
         for (let j = 0; j < arr1[i].length; j++) {
-            newArr1.push(arr1[i][j] + arr2[i][j]);
+            answer[i].push(arr1[i][j] + arr2[i][j]);
         }
-        answer.push(newArr1);
     }
     return answer;
 }
