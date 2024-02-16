5650126fe2734b72f49b0d89147c9d447cae52c3
camel <157693710+coco-camel@users.noreply.github.com>
2024-02-17 00:47:14 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.10 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,14 @@
+function solution(strings, n) {
+    let answer = [];
+    answer = strings.sort((a,b)=>{
+        if(a[n] > b[n]) return 1;
+        if(b[n] > a[n]) return -1;
+
+        if(a > b) return 1;
+        if(b > a) return -1;
+
+        return 0;
+    });
+    
+    return answer;
+}
