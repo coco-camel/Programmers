366e18c4e69482311573089917d90a80d847e945
camel <157693710+coco-camel@users.noreply.github.com>
2024-02-17 17:25:34 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,7 +1,7 @@
 function solution(absolutes, signs) {
     var answer = 0;
     for(let i = 0; i < absolutes.length; i++) {
-            if(signs[i] === true) {
+            if(signs[i]) {
                 answer+=absolutes[i];
             } else {
                 answer-=absolutes[i];
