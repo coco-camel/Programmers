36589309cba0eb6f610e23e0068317dfe5a3a183
camel <157693710+coco-camel@users.noreply.github.com>
2024-02-18 02:45:23 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.21 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,25 @@
+function solution(X, Y) {
+let answer = {}
+let result = ''
+const x = Array.from(X)
+const y = Array.from(Y)
+
+for( let i = 0; i <= 9; i++){
+const xLen = x.filter((e)=> Number(e) === i)
+const yLen = y.filter((e)=> Number(e) === i)
+if( Math.min(xLen.length, yLen.length) !== 0 ){
+answer[i] = Math.min(xLen.length, yLen.length)
+}
+}
+for (const [key, value] of Object.entries(answer)) {
+result += key.repeat(value);
+}
+result = result.split('').sort((a, b) => b > a ? 1 : -1)
+
+if (Number(result.join('')) === 0 && result.length !== 0)  {
+    result = ["0"];
+}
+
+
+return result.length ? result.join('') : '-1'
+}
